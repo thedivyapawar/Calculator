501dcf38609b6fe3393c59ab83bdd9b0b9d16c94
Divya Pawar <divyapawar2033@gmail.com>
2024-10-19 18:02:58 +0530

    deploy

---
+++ main.js
@@ -0,0 +1,28 @@
+const input = document.querySelector(".input");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+
+const calculate = (btnValue) => {
+  input.focus();
+  if (btnValue === "=" && output !== "") {
+   
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+  
+    output = output.toString().slice(0, -1);
+  } else {
+    
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  input.value = output;
+};
+
+buttons.forEach((button) => {
+ 
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
